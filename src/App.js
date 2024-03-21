c22e7145165ae1674b4329b62002520fa497f49f
ayseayse0346 <ayseayse.ayse0346@gmail.com>
2024-03-21 18:35:23 +0300

    jsx dosyaları açıldı

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import Footer from "./Footer";
+import Header from "./Header";
+import Main from "./Main";
 
 
 
@@ -5,6 +8,10 @@ function App() {
   return (
     <div>
       <h1>APP</h1>
+      {/* Header component import edildi. */}
+      <Header/>
+      <Main/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,17 @@
+
+// ?Header component 
+
+function Header(){
+    //! js alanı 
+    const yazı = "hello fs 16"
+    return (
+    // jsx alanı
+    <header>
+        {/* JSX alanı yorum satırı */}
+        <h1>{yazı}</h1>
+    </header>
+    
+    )
+}
+
+export default Header
